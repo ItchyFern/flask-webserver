c134e815a842a13fc674550e6e5feb497caff64a
Seth Hannah <46728639+ItchyFern@users.noreply.github.com>
2022-11-15 14:10:26 -0500

    add unsecure copy to clipboard

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -11,8 +11,26 @@ function myFunction() {
     copyText.setSelectionRange(0, 99999); // For mobile devices
   
      // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
+     try {
+        navigator.clipboard.writeText(copyText.value);
+      } catch (err) {
+        unsecuredCopyToClipboard(copyText.value);
+      }
+    
   
     // Alert the copied text
     alert("Copied the text: " + copyText.value);
+  }
+  function unsecuredCopyToClipboard(text) {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+      document.execCommand('copy');
+    } catch (err) {
+      console.error('Unable to copy to clipboard', err);
+    }
+    document.body.removeChild(textArea);
   }
